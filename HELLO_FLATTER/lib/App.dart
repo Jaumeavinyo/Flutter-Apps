ed803c634f3ecdf7215799f4c4bc33418927c16d
EnricGDV <minenime4thewin@gmail.com>
2020-11-04 00:38:30 +0100

    Background

--- HELLO_FLATTER/lib/App.dart
+++ HELLO_FLATTER/lib/App.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Hello Fluuter",
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: HomeScreen(),
     );
   }
@@ -16,11 +16,45 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter Hello World"),
+      backgroundColor: Colors.blue.shade700,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 2,
+          ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.end, children: [
+            Expanded(
+              flex: 2,
+              child: Container(color: Colors.blue.shade700),
+            ),
+            Expanded(
+              flex: 8,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                ),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.blue.shade700,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(15),
+                        topRight: Radius.circular(15),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ]),
         ),
-        body: Center(
-          child: Text("hello!"),
-        ));
+      ),
+    );
   }
 }
