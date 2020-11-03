96626fb728f7da9daab374ae66e5b11f22c4618f
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2020-11-03 22:30:42 +0100

    base code upload

---
+++ HELLO_FLATTER/android/app/src/main/kotlin/com/example/HELLO_FLATTER/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.HELLO_FLATTER
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ HELLO_FLATTER/lib/App.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Hello Fluuter",
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Flutter Hello World"),
+        ),
+        body: Center(
+          child: Text("hello!"),
+        ));
+  }
+}
