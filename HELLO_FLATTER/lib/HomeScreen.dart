e8a5f29bdcd2dc592b124cc557dde4d8f114aea7
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2020-11-08 19:01:55 +0100

    merge done jaume to enric

--- HELLO_FLATTER/lib/HomeScreen.dart
+++ HELLO_FLATTER/lib/HomeScreen.dart
@@ -14,15 +14,124 @@ class HomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.blue.shade700,
       body: SafeArea(
-        child: Stack(
-          children: [
-            CreditCardWidgetComplete(
-                nameVar: userHomeVar.card.name,
-                surnameVar: userHomeVar.card.surname,
-                creditCardNumberVar: userHomeVar.card.creditCardNumber,
-                totalMoneyVar: userHomeVar.card.money,
-                expirationDateVar: userHomeVar.card.expirationDate)
-          ],
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 2,
+          ),
+          child: Stack(
+            children: [
+              Column(
+                children: [
+                  Expanded(
+                    flex: 24,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Expanded(
+                          flex: 5,
+                          child: Container(
+                            color: Colors.blue[700],
+                          ),
+                        ),
+                        Expanded(
+                          flex: 5,
+                          child: Container(
+                            color: Colors.blue[800],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 75,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(15),
+                          topRight: Radius.circular(15),
+                        ),
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          top: 76,
+                          left: 10,
+                          right: 10,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            CustomTitle(text: "My Wallets"),
+                            Spacer(flex: 20),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                AddWallet(),
+                                Spacer(flex: 1),
+                                Wallet(
+                                  color: Colors.orangeAccent[700],
+                                  currency: "Bitcoin",
+                                  quantity: 0.23456,
+                                  percentage: 2.5,
+                                  isPositive: true,
+                                  isShadowLeft: true,
+                                  isShadowRight: false,
+                                ),
+                                Spacer(flex: 2),
+                                Wallet(
+                                  color: Colors.purple[900],
+                                  currency: "Ethereum",
+                                  quantity: 0.23456,
+                                  percentage: 0.5,
+                                  isPositive: true,
+                                  isShadowLeft: false,
+                                  isShadowRight: true,
+                                ),
+                              ],
+                            ),
+                            Spacer(flex: 14),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                TransactionButton(
+                                  color: Colors.tealAccent[400],
+                                  action: 'Buy',
+                                  icon: Icons.shopping_cart,
+                                ),
+                                TransactionButton(
+                                  color: Colors.pinkAccent[200],
+                                  action: 'Sell',
+                                  icon: Icons.shopping_basket,
+                                ),
+                              ],
+                            ),
+                            Spacer(flex: 12),
+                            CustomTitle(
+                              text: "Transaction",
+                              hasViewAll: false,
+                            ),
+                            Spacer(flex: 5),
+                            Expanded(
+                              flex: 32,
+                              child: Transaction(),
+                            ),
+                            Spacer(flex: 50),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              WindowBar(),
+              CreditCardWidgetComplete(
+                  nameVar: userHomeVar.card.name,
+                  surnameVar: userHomeVar.card.surname,
+                  creditCardNumberVar: userHomeVar.card.creditCardNumber,
+                  totalMoneyVar: userHomeVar.card.money,
+                  expirationDateVar: userHomeVar.card.expirationDate)
+            ],
+          ),
         ),
       ),
     );
