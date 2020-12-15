e34bf3e697f2eaf445fd1057c72f9c12bfc9f01b
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2020-12-16 00:01:46 +0100

    first button widget working

--- FLUTTER_TEMPLATE/lib/App.dart
+++ FLUTTER_TEMPLATE/lib/App.dart
@@ -16,21 +16,167 @@ class HomeScreen extends StatelessWidget {
                 style: TextStyle(fontSize: 30),
               ),
               SizedBox(
-                height: 40,
-                child: RaisedButton(
-                  child: Text("hello"),
-                  onPressed: () {
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (_) => SecondScreen(),
-                      ),
-                    );
-                  },
-                ),
-              )
+                height: 120,
+              ),
+              Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: GreenButton(),
+                        /*
+                        child: RaisedButton(
+                          child: Text("example text"),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (_) => SecondScreen(),
+                              ),
+                            );
+                          },
+                        ),*/
+                      )
+                    ],
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: RaisedButton(
+                          child: Text("example text"),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (_) => SecondScreen(),
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  )),
+              Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: RaisedButton(
+                          child: Text("example text"),
+                          onPressed: () {
+                            Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (_) => SecondScreen(),
+                              ),
+                            );
+                          },
+                        ),
+                      )
+                    ],
+                  ))
+              /*RaisedButton(
+                child: Text("Calendar"),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => SecondScreen(),
+                    ),
+                  );
+                },
+              ),
+              RaisedButton(
+                child: Text("Your aquarium"),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => SecondScreen(),
+                    ),
+                  );
+                },
+              ),
+              RaisedButton(
+                child: Text("Forum"),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => SecondScreen(),
+                    ),
+                  );
+                },
+              ),*/
             ],
           ),
         ));
+  }
+}
+
+class GreenButton extends StatelessWidget {
+  num radiusVar = 10;
+  String buttonTextVar = "example text 1";
+  Color colorVar = Colors.green;
+  Color borderColorVar = Colors.black;
+  num borderWidthVar = 2;
+  num textLeftSpaceVar = 20;
+  GreenButton({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(50),
+      decoration: BoxDecoration(
+        color: colorVar,
+        border: Border.all(
+          color: borderColorVar,
+          width: borderWidthVar,
+        ),
+        borderRadius: BorderRadius.all(Radius.circular(radiusVar)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(
+                width: textLeftSpaceVar,
+              ),
+              Text("$buttonTextVar"),
+              Expanded(
+                  flex: 2,
+                  child: SizedBox(
+                    width: 1,
+                  )),
+              FlatButton(
+                child: Text(
+                  ">",
+                  style: TextStyle(fontSize: 30),
+                ),
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute(
+                      builder: (_) => SecondScreen(),
+                    ),
+                  );
+                },
+              ),
+              Expanded(
+                  flex: 1,
+                  child: SizedBox(
+                    width: 1,
+                  )),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 
