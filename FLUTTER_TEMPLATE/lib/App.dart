65fa76b04bb4dbc18615d650fd9be784bbba2cd8
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2020-12-15 22:54:39 +0100

    just a base to work with

--- FLUTTER_TEMPLATE/lib/App.dart
+++ FLUTTER_TEMPLATE/lib/App.dart
@@ -5,8 +5,42 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue.shade700,
-      //body: SafeArea(),
+        backgroundColor: Colors.blue.shade700,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Text(
+                "Welcome back Jaume",
+                style: TextStyle(fontSize: 30),
+              ),
+              SizedBox(
+                height: 40,
+                child: RaisedButton(
+                  child: Text("hello"),
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => SecondScreen(),
+                      ),
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ));
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("choose a color"),
+      ),
     );
   }
 }
